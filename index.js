ec6412b87ed7601d5ecdfa9a9392e8c26d0a5c35
Nicole D <nnardella@gmail.com>
2018-08-20 15:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase () === 'string')
    return ("I can't hear you!")
    
-} else if (string.toUpperCase () === 'HELLO') {
+} else if (string.toUpperCase () === 'string') {
     return ("YES INDEED!")
 
   
