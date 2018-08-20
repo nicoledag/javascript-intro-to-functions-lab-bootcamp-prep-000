5a842f0920b0fbf033a8ad15069230fbdcc59c1b
Nicole D <nnardella@gmail.com>
2018-08-20 14:51:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+ 
+  if (string.toLowerCase () === lowercase)
+   return ("I can't hear you!")
+   
+} else if (string.toUpperCase () === uppercase) {
+    return ("YES INDEED!")
+}
+
+
+
+ 
+ 
+
+ 
+
+ 
