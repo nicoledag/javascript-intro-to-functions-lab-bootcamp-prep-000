02a5e3d03c769a6196cd4c4391acd25d5bf545e6
Nicole D <nnardella@gmail.com>
2018-08-20 15:24:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,17 +16,18 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
  
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
    return ("I can't hear you!")
    
 } else if (string.toUpperCase() === string) {
     return ("YES INDEED!")
 
   
-} if (string === 'I love you, Grandma') {
+} else if (string === 'I love you, Grandma') {
     return ("I love you, too")
 }
 
+}
 
 
  
